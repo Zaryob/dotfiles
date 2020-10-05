7bd3ecdd30a309af6abd3eb41c94e03bf177c513
Suleyman Poyraz <zaryob.dev@gmail.com>
2020-10-05 14:05:08 +0300

    i3 simplicity temasında ufak düzeltmeler yapıldı.

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 if [ ! -f "$HOME/.zaryob" ]; then
     echo "Installing Zaryob's dotfiles."
     git clone --depth=1 https://github.com/Zaryob/dotfiles.git "$HOME/.dotfiles"
-    cd "$HOME/.dotfiles"    
+    cd "$HOME/.dotfiles"
     printf "\033[0;31m* Setting shells.\033[0m\n"
     ln -svf $HOME/.dotfiles/shell/aliases $HOME/.aliases
     ln -svf $HOME/.dotfiles/shell/inputrc $HOME/.inputrc
@@ -11,8 +11,8 @@ if [ ! -f "$HOME/.zaryob" ]; then
     ln -svf $HOME/.dotfiles/shell/bash/profile $HOME/.profile
     printf "\033[0;32m + Linked bash configs\033[0m\n"
     printf "\033[0;33m - Generating zsh links\033[0m\n"
-    ln -svf $HOME/.dotfiles/shell/zsh/zshrc $HOME/.zshrc   
-    ln -svf $HOME/.dotfiles/shell/zsh/zlogout $HOME/.zlogout  
+    ln -svf $HOME/.dotfiles/shell/zsh/zshrc $HOME/.zshrc
+    ln -svf $HOME/.dotfiles/shell/zsh/zlogout $HOME/.zlogout
     ln -svf $HOME/.dotfiles/shell/zsh/zprofile $HOME/.zprofile
     printf "\033[0;32m + Linked bash configs\033[0m\n"
     printf "\033[0;33m - Installing vim plugins\033[0m\n"
@@ -32,17 +32,14 @@ if [ ! -f "$HOME/.zaryob" ]; then
     printf "\033[0;32m + Copied fonts\033[0m\n"
     printf "\033[0;33m - Setting i3wm up\033[0m\n"
     [ -f $HOME/.config/i3 ] || rm -rf $HOME/.config/i3
-    mkdir -v $HOME/.config/i3
-    ln -svf $HOME/.dotfiles/i3-themes/simplicity/config/clipit  $HOME/.dotfiles/i3-themes/simplicity/config/i3  $HOME/.config/i3
-    
+    [ -f $HOME/.config/clipit ] || rm -rf $HOME/.config/clipit
+    ln -svf $HOME/.dotfiles/i3-themes/simplicity/config/clipit  $HOME/.dotfiles/i3-themes/simplicity/config/i3  $HOME/.config/
     ln -svf $HOME/.dotfiles/i3-themes/simplicity/fehbg $HOME/.fehbg
     printf "\033[0;32m + i3wm setted up\033[0m\n"
     printf "\033[0;33m - Initializing git \033[0m\n"
     mkdir -pv $HOME/.local/share/man/man1
     cp -vf $HOME/.dotfiles/git/man/man1/* $HOME/.local/share/man/man1
-    [ -f $HOME/.git ] || rm -rf $HOME/.git
-    mkdir -v $HOME/.git
-    ln -svf $HOME/.dotfiles/git/commands $HOME/.git/commands
+    ln -svf $HOME/.dotfiles/git/commands $HOME/.gitbin
     ln -svf $HOME/.dotfiles/git/gitconfig $HOME/.gitconfig
     ln -svf $HOME/.dotfiles/git/gitmessage $HOME/.gitmessage
     printf "\033[0;32m + Initialized git \033[0m\n"
